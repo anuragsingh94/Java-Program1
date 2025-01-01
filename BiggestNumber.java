316b63f82c09c2b4bd49453b898267fa36fed584
anuragsingh94 <anuragsingh945132@gmail.com>
2025-01-01 16:00:56 +0530

    Add files via upload
    1.if statements are used to check conditions. Each condition compares the numbers to determine which is the Biggest.
    2.The Scanner class is used for taking input from the user.
    3.Logical operators like >= and && are used to compare numbers.

---
+++ BiggestNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BiggestNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = input.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int num2 = input.nextInt();
+        
+        System.out.print("Enter the third number: ");
+        int num3 = input.nextInt();
+        
+        // Check which number is the Biggest using if-else conditions
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("The largest number is: " + num1);
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("The largest number is: " + num2);
+        } else {
+            System.out.println("The largest number is: " + num3);
+        }
+    }
+}
